9cd716d63212f2f394b0ffd309a41101a9fffa5a
Andreas Barthel <andreas.barthel@outlook.com>
2017-12-15 22:43:30 +0100

    fixed typos in exampe sql

--- example-rule.sql
+++ example-rule.sql
@@ -1,5 +1,5 @@
 USE `smarthome`;
 # Exporting data from `smarthome`
 # Data for table `smarthome`.`rule`:
-INSERT INTO `smarthome`.`rule` VALUES ('toggleLigh', '60:01:94:3c:c8:2b', 'switchStateChanged', '2c:3a:e8:0b:58:ac', 'toggleBoolean', 1);
+INSERT INTO `smarthome`.`Rule` VALUES ('toggleLight', '60:01:94:3c:c8:2b', 'switchStateChanged', '2c:3a:e8:0b:58:ac', 'toggleBoolean', 1);
 #...done.
