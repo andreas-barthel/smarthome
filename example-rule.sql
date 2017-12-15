3d6d04625d55b324f8c9cd0ebdc121a1cf1d6a1b
Andreas Barthel <andreas.barthel@outlook.com>
2017-12-15 22:29:15 +0100

    added example rule sql file

---
+++ example-rule.sql
@@ -0,0 +1,5 @@
+USE `smarthome`;
+# Exporting data from `smarthome`
+# Data for table `smarthome`.`rule`:
+INSERT INTO `smarthome`.`rule` VALUES ('toggleLigh', '60:01:94:3c:c8:2b', 'switchStateChanged', '2c:3a:e8:0b:58:ac', 'toggleBoolean', 1);
+#...done.
